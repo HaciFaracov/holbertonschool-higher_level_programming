066177c234726071e736499b86957691b662ad80
Haji <hacih4451@gmail.com>
2026-02-15 18:07:10 +0400

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,11 +2,9 @@
 def new_in_list(my_list, idx, element):
     # Always create a copy first
     copy = my_list[:]
-    
     # Check boundaries
     if idx < 0 or idx >= len(my_list):
         return copy
-    
     # Modify the copy, not the original
     copy[idx] = element
     return copy
