727fa84feac408b5c1c182c7948588e0116d6c9a
Haji <hacih4451@gmail.com>
2026-02-22 02:31:24 +0400

    Update 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    
     for i in range(list_length):
         div_result = 0
         try:
@@ -14,5 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list.append(div_result)
-            
     return new_list
