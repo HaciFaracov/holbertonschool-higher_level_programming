4193194f17c169d35738277bf149a59fef08e513
HaciFaracov <hacih4451@gmail.com>
2026-02-22 01:46:24 +0000

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+"""
+This module contains a function that adds two numbers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats.
+    Raises a TypeError if a or b are not integers or floats.
+    Casts floats to integers before adding.
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
