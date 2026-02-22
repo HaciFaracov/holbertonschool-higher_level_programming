820dee0b983d831d56e44d1cbc4342b3501ae1f3
Haji <hacih4451@gmail.com>
2026-02-22 16:55:51 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,15 +8,12 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     special = (".", "?", ":")
     i = 0
-    # Requirement: No space at the beginning or end of printed lines
     text = text.strip()
     while i < len(text):
         print(text[i], end="")
         if text[i] in special:
-            # This prints exactly two newlines (one blank line)
-            print("\n", end="")
+            print("\n")
             i += 1
-            # Skip all spaces following the punctuation
             while i < len(text) and text[i] == ' ':
                 i += 1
             continue
