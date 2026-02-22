849a9e36644f0c779cef84a280e82355a23c4a14
Haji <hacih4451@gmail.com>
2026-02-22 16:58:42 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""Module for text indentation"""
+"""
+This module contains a function that indents text based on punctuation.
+"""
 
 
 def text_indentation(text):
-    """Prints text with 2 newlines after punctuation"""
+    """
+    Prints a text with 2 new lines after punctuation: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    special = (".", "?", ":")
+
+    special = ('.', '?', ':')
     i = 0
+    # Requirement: No space at the beginning or end of printed lines
     text = text.strip()
+
     while i < len(text):
         print(text[i], end="")
         if text[i] in special:
             print("\n")
             i += 1
+            # Skip all spaces following the punctuation
             while i < len(text) and text[i] == ' ':
                 i += 1
             continue
