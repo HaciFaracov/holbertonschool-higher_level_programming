0357264fc23bbbbed562e1b5c5087398ff4861e2
Haji <hacih4451@gmail.com>
2026-02-22 18:41:26 +0400

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,56 +1,68 @@
-5. Printing a square
-Write a class Square that defines a square by: (based on 4-square.py)
-
-Private instance attribute: size:
-property def size(self): to retrieve it
-property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
-Instantiation with optional size: def __init__(self, size=0):
-Public instance method: def area(self): that returns the current square area
-Public instance method: def my_print(self): that prints in stdout the square with the character #:
-if size is equal to 0, print an empty line
-You are not allowed to import any module
-guillaume@ubuntu:~/$ cat 5-main.py
 #!/usr/bin/python3
-Square = __import__('5-square').Square
+"""
+This module defines a Square class that can print its visual representation.
+It builds on previous tasks to add a public printing method.
+"""
 
-my_square = Square(3)
-my_square.my_print()
 
-print("--")
+class Square:
+    """
+    A class that defines a square by its size and provides a print method.
+    """
 
-my_square.size = 10
-my_square.my_print()
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
 
-print("--")
+        Args:
+            size (int): The side length of the square.
+        """
+        self.size = size
 
-my_square.size = 0
-my_square.my_print()
+    @property
+    def size(self):
+        """
+        Retrieves the private size attribute of the square.
 
-print("--")
+        Returns:
+            int: The current size of the square.
+        """
+        return self.__size
 
-guillaume@ubuntu:~/$ ./5-main.py
-###
-###
-###
---
-##########
-##########
-##########
-##########
-##########
-##########
-##########
-##########
-##########
-##########
---
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size attribute with strict type and value validation.
 
---
-guillaume@ubuntu:~/$ 
-Repo:
+        Args:
+            value (int): The new size of the square.
 
-GitHub repository: holbertonschool-higher_level_programming
-Directory: python-classes
-File: 5-square.py
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the area of the current square.
+
+        Returns:
+            int: The area (size squared).
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints the square in the stdout using the # character.
+        If size is 0, it prints an empty line.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
