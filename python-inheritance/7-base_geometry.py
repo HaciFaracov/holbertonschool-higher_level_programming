02599f7d42092dfa652c9c393712c7e1cd7d731c
Haji <hacih4451@gmail.com>
2026-03-01 13:05:29 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""
-This module defines a Rectangle class that inherits from BaseGeometry.
-"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Defines a base geometry class BaseGeometry."""
 
 
-class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle, inheriting from BaseGeometry.
-    """
+class BaseGeometry:
+    """Reprsent base geometry."""
 
-    def __init__(self, width, height):
-        """
-        Initializes a new Rectangle instance.
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
 
         Args:
-            width (int): The horizontal dimension.
-            height (int): The vertical dimension.
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
