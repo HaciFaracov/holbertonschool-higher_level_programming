ee3aff1bdbf6365d65a97cfff46fe381e44cf38e
Haji <hacih4451@gmail.com>
2026-03-07 21:09:20 +0400

    Create 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script adds all command-line arguments to a Python list
+and saves them to a file named add_item.json.
+"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+# sys.argv[1:] captures all arguments after the script name
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
