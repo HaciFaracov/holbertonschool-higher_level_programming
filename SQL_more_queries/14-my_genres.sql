60071ed244888cc87c00aa2607636bcb7a7c734b
Haji <hacih4451@gmail.com>
2026-03-14 18:14:00 +0400

    Remove ORDER BY clause from my_genres query

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,14 +5,3 @@ INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
-```
-
----
-
-### 📖 Explanation
-
-This time we need data from **3 tables** at once, so we use **two JOINs chained together**!
-
-Think of it like a chain:
-```
-tv_genres → tv_show_genres → tv_shows
