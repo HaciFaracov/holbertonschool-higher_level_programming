ae9ceef8088036aef1f6219f0efb1c5e47cbcd85
Haji <hacih4451@gmail.com>
2026-03-15 17:15:25 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-
-"""
-This script connects to a MySQL database and retrieves all rows
-from the 'states' table, ordered by their ID in ascending order.
-
-Usage:
-    ./script_name.py <username> <password> <database_name>
-
-Example:
-    ./0-select_states.py root mypassword hbtn_0e_0_usa
-
-Modules:
-    sys - to retrieve command-line arguments
-    MySQLdb - to connect and interact with a MySQL database
-"""
+"""Lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-
-    av = sys.argv[1:]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=av[0],
-        passwd=av[1],
-        db=av[2],
-        charset="utf8",
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
     )
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
